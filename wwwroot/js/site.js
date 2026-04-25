b9d7a4defa683de7af0e68f7b3da52726ffef36a
jan lorenz <larocojanlorenz@gmail.com>
2026-04-26 00:49:38 +0800

    ubos ang token

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,44 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿(function () {
+	var menuToggle = document.querySelector("[data-menu-toggle]");
+	var menuPanel = document.querySelector("[data-menu-panel]");
 
-// Write your JavaScript code.
+	if (!menuToggle || !menuPanel) {
+		return;
+	}
+
+	var desktopMinWidth = 834;
+
+	function closeMenu() {
+		menuPanel.classList.remove("is-open");
+		menuToggle.setAttribute("aria-expanded", "false");
+	}
+
+	function openMenu() {
+		menuPanel.classList.add("is-open");
+		menuToggle.setAttribute("aria-expanded", "true");
+	}
+
+	menuToggle.addEventListener("click", function () {
+		if (menuPanel.classList.contains("is-open")) {
+			closeMenu();
+			return;
+		}
+
+		openMenu();
+	});
+
+	document.addEventListener("click", function (event) {
+		var target = event.target;
+		if (menuPanel.contains(target) || menuToggle.contains(target)) {
+			return;
+		}
+
+		closeMenu();
+	});
+
+	window.addEventListener("resize", function () {
+		if (window.innerWidth >= desktopMinWidth) {
+			closeMenu();
+		}
+	});
+})();
